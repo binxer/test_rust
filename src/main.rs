91e2f6ab37aa330c9d3212ca0845e1fea7797ffd
binxer <binx@mail.ru>
2026-01-13 21:24:12 +0400

    message: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::clone;
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn newPoint<'a>(Point: &'a Point) -> &'a Point {
+    &Point
+}
+fn main() {
+    let mut p = Point { x: 1, y: 2 };
+    match &mut p {
+        &mut Point { ref mut x, ref y } => {
+            *x += 1;
+        }
+    }
+
+    println!("Point({}, {})", p.x, p.y);
+    let numbers = vec![1, 2, 3, 4, 5];
+    let sum = numbers.iter().fold(0, |a, &num| a + num);
+    println!("Sum: {}", sum);
+}
